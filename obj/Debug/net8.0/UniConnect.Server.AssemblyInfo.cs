9f56f617a1f0ebee3cb02cd5dc333686423b5347
Pedro Lagemann <pedro.lagmann6904@gmail.com>
2025-11-12 23:39:13 -0300

    Correcao Redirect

--- obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec43f7402d68676ee69305cbe5be86d5b94bb951")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d15f03d5867af4690836c2f6520e3b6f28882cc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
