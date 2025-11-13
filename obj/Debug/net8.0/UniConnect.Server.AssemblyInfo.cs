00c27d2dfb1d6f717a7e278f4a93b819f7a44f6e
Pedro Lagemann <pedro.lagmann6904@gmail.com>
2025-11-12 22:36:12 -0300

    correcao render.yaml

--- obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23cd340f2dbd2a5de128945730632eadf663c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b630c51bf502614dc59becefbd5510472cec6f6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
