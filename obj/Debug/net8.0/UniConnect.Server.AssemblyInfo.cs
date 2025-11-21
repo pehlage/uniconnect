4b71868639ee225b61590de1b6e926838522957c
Pedro Lagemann <pedro.lagmann6904@gmail.com>
2025-11-20 23:27:15 -0300

    SideBar Lateral Responsiva

--- obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f27095dd96c31ce77fad6d11918b9403d297b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9824948fc3f4d7f79224ceebe88171919428dad")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
