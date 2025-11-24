01dd0d0d018a7855a73dd4199516ce49bd4da95b
Pedro Lagemann <pedro.lagmann6904@gmail.com>
2025-11-24 15:05:19 -0300

    Correcao Definitiva Css

--- obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0fd994f5de44869e1580c1c912eec359a6cc79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f0151a3994c5b9628b664464435bea9edc69ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
