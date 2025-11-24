fe0fd994f5de44869e1580c1c912eec359a6cc79
Pedro Lagemann <pedro.lagmann6904@gmail.com>
2025-11-24 14:16:51 -0300

    Inclusao do Logo Oficial

--- obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/UniConnect.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3aeda7c354ab4c37ce5ea632cf6f8659b921eb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+629b56deb14c10864c2ae09e6d0fd885c171fd4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniConnect.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
